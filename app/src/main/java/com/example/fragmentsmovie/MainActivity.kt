982a627c42f5f92f1f3c89415bee2e236506d95a
Sergey Belov <sergpc@mail.ru>
2020-05-17 23:27:48 +0300

    Homework fragments inet

--- app/src/main/java/com/example/fragmentsmovie/MainActivity.kt
+++ app/src/main/java/com/example/fragmentsmovie/MainActivity.kt
@@ -26,8 +26,7 @@ class MainActivity : AppCompatActivity(), MoviesListFragments.MoviesListListener
         var allFilmsFragmentAttached: Fragment = Fragment()
         var numLastAddIndex:Int =0
     }
-
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
